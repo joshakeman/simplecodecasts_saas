68487c95cbea1f5b0f137241c917721a27256fa6
Josh Akeman <joshakeman@gmail.com>
2016-05-11 14:53:31 +0000

    Added actionmailer settings again

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-   default to: 'joshakeman@gmail.com'
+    default to: 'joshakeman@gmail.com'
    
     def contact_email(name, email, body)
        @name = name
