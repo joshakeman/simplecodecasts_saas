36bb8662973c2d3b2284e3ef9f4873289f0adff3
Josh Akeman <joshakeman@gmail.com>
2016-05-24 14:59:02 +0000

    Fixed typo in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,4 @@
-class PagesController < ApplicationController
+class ProfilesController < ApplicationController
     def new
         # form where a user can fill out their own profile.
         @user = User.find( params[:user_id] )
